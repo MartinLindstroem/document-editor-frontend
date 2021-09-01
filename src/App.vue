711e6d6d355bce38be535e3fa07d857819bb8d02
Martin Lindström <martinllindstroem@gmail.com>
2021-09-01 11:33:01 +0200

    cleaned out template code

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,18 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <TextEditor />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TextEditor from "./components/TextEditor.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    TextEditor,
+  },
+};
 </script>
 
 <style>
