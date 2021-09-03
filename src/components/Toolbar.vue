041de7101069dce3b861d6a176be77e68efbf900
Martin Lindström <martinllindstroem@gmail.com>
2021-09-03 14:19:12 +0200

    created component for toolbar

---
+++ src/components/Toolbar.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="toolbar">
+    <button class="btn" v-on:click="log">Save</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Toolbar",
+  props: ["editorContent"],
+  methods: {
+    log: function () {
+      console.log(this.editorContent);
+    },
+  },
+};
+</script>
+
+<style>
+.toolbar {
+  display: flex;
+  justify-content: flex-start;
+  width: 100%;
+  height: 30px;
+  background-color: rgb(81, 81, 131);
+  margin-bottom: 20px;
+  padding: 10px;
+}
+
+.btn {
+  border-radius: 15%;
+}
+
+.btn:hover {
+  background-color: #ddd;
+  cursor: pointer;
+}
+</style>
+
