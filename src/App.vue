7ec7de6988d38857928dd02e342298cd9e0347af
Martin Lindström <martinllindstroem@gmail.com>
2021-09-03 14:18:43 +0200

    updated components

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div id="app">
+    <!-- <Toolbar /> -->
     <TextEditor />
   </div>
 </template>
 
 <script>
 import TextEditor from "./components/TextEditor.vue";
+// import Toolbar from "./components/Toolbar.vue";
 
 export default {
   name: "App",
   components: {
     TextEditor,
+    // Toolbar,
   },
 };
 </script>
@@ -20,8 +23,8 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  /* text-align: center; */
   color: #2c3e50;
-  margin-top: 60px;
+  /* margin-top: 60px; */
 }
 </style>
--- src/components/TextEditor.vue
+++ src/components/TextEditor.vue
@@ -1,10 +1,37 @@
 <template>
-  <h1>HELLO</h1>
+  <div>
+    <toolbar :editorContent="this.editorContent" />
+    <VueTrix v-model="editorContent" autofocus localStorage />
+  </div>
 </template>
 
 <script>
+import VueTrix from "vue-trix";
+import Toolbar from "@/components/Toolbar.vue";
 export default {
   name: "TextEditor",
+  components: {
+    VueTrix,
+    Toolbar,
+  },
+  data: function () {
+    return {
+      editorContent: "",
+    };
+  },
+  methods: {
+    updateEditorContent(value) {
+      this.editorContent = value;
+    },
+  },
+  watch: {
+    editorContent: {
+      handler: "updateEditorContent",
+    },
+  },
 };
 </script>
+
+<style scoped>
+</style>
 
