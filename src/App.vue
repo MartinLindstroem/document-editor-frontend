a7628a16f56a7ac8c81c64b72bd1536ae5ab6765
Martin Lindström <martinllindstroem@gmail.com>
2021-09-13 09:28:21 +0200

    added variables and methods to pass data between components

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,57 @@
 <template>
   <div id="app">
-    <!-- <Toolbar /> -->
-    <TextEditor />
+    <Toolbar
+      :editorContent="textContent"
+      :documentName="docName"
+      :documents="docsList"
+      @select="selectDocument"
+      @inputData="updateTextContent"
+      @inputName="updateDocName"
+    />
+    <TextEditor
+      @inputData="updateTextContent"
+      @inputName="updateDocName"
+      :selectedDoc="selectedDoc"
+    />
   </div>
 </template>
 
 <script>
 import TextEditor from "./components/TextEditor.vue";
-// import Toolbar from "./components/Toolbar.vue";
+import Toolbar from "./components/Toolbar.vue";
 
 export default {
   name: "App",
   components: {
     TextEditor,
-    // Toolbar,
+    Toolbar,
+  },
+  data: function () {
+    return {
+      textContent: "",
+      docName: "",
+      selectedDoc: "",
+      docsList: "",
+    };
+  },
+  methods: {
+    updateTextContent(value) {
+      this.textContent = value;
+    },
+    updateDocName(value) {
+      this.docName = value;
+    },
+    selectDocument(value) {
+      this.selectedDoc = value;
+    },
+    getAllDocuments: function () {
+      fetch("https://jsramverk-editor-mamv18.azurewebsites.net/getAll")
+        .then((response) => response.json())
+        .then((data) => (this.docsList = JSON.parse(JSON.stringify(data))));
+    },
+  },
+  beforeMount() {
+    this.getAllDocuments();
   },
 };
 </script>
