f446d59da4e315a20c316c9ebbd75bd6c78bb7c0
Martin Lindström <martinllindstroem@gmail.com>
2021-09-13 09:32:00 +0200

    added input for document name, added methods to emit document data

--- src/components/TextEditor.vue
+++ src/components/TextEditor.vue
@@ -1,37 +1,59 @@
 <template>
   <div>
-    <toolbar :editorContent="this.editorContent" />
-    <VueTrix v-model="editorContent" autofocus localStorage />
+    <form>
+      <label for="docName">Document name</label><br />
+      <!-- <input v-model="documentName" type="text" id="docName" /> -->
+      <input
+        v-if="selectedDoc"
+        v-model="selectedDoc.name"
+        type="text"
+        id="docName"
+      />
+      <input v-else v-model="documentName" type="text" id="docName" />
+      <VueTrix v-if="selectedDoc" v-model="selectedDoc.content" autofocus />
+      <VueTrix v-else v-model="editorContent" autofocus />
+    </form>
   </div>
 </template>
 
 <script>
 import VueTrix from "vue-trix";
-import Toolbar from "@/components/Toolbar.vue";
 export default {
   name: "TextEditor",
   components: {
     VueTrix,
-    Toolbar,
   },
+  props: ["selectedDoc"],
   data: function () {
     return {
       editorContent: "",
+      documentName: "",
     };
   },
   methods: {
-    updateEditorContent(value) {
-      this.editorContent = value;
+    updateEditorContent() {
+      this.$emit("inputData", this.editorContent);
+    },
+    updateDocName() {
+      this.$emit("inputName", this.documentName);
     },
   },
   watch: {
     editorContent: {
       handler: "updateEditorContent",
     },
+    documentName: {
+      handler: "updateDocName",
+    },
   },
 };
 </script>
 
 <style scoped>
+#docName {
+  margin-bottom: 15px;
+  height: 20px;
+  padding: 5px 5px;
+}
 </style>
 
