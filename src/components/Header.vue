ed0b21cf7f18c2a73c8a3cca8cc65b508833ac3c
Martin Lindström <martinllindstroem@gmail.com>
2021-10-01 17:14:20 +0200

    added header component

---
+++ src/components/Header.vue
@@ -0,0 +1,42 @@
+<template>
+  <nav class="navbar">
+    <ul class="nav-content">
+      <li><router-link class="nav-link" to="/">Home</router-link></li>
+      <li><router-link class="nav-link" to="/editor">Editor</router-link></li>
+      <li>
+        <router-link class="nav-link" to="/login">Login</router-link>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {};
+  },
+};
+</script>
+
+<style>
+.navbar {
+  background-color: #2196f3;
+  margin-bottom: 20px;
+}
+.nav-content {
+  padding: 30px;
+}
+.nav-content li {
+  display: inline;
+  padding: 10px;
+  color: white;
+  font-size: 20px;
+}
+.nav-content li a {
+  color: white;
+  text-decoration: none;
+  padding: 10px;
+  /* float: left; */
+  /* float: right; */
+}
+</style>
