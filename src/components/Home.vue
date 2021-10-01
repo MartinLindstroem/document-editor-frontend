50f79e9289db1e2ff2f2c0d086f59093bfb7865e
Martin Lindström <martinllindstroem@gmail.com>
2021-10-01 17:14:41 +0200

    added home component

---
+++ src/components/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>Hello there</h1>
+    <p>Sign in to user the editor</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {};
+  },
+};
+</script>
+
+<style>
+</style>
